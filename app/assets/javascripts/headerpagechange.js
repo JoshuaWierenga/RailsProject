3b36a064d8c57e36d62646839c83d1b60a8a34b0
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2015-12-14 00:31:10 +1100

    added a css icon

--- app/assets/javascripts/headerpagechange.js
+++ app/assets/javascripts/headerpagechange.js
@@ -1,23 +1,24 @@
 function headerchangebookmarks()
 {
   document.getElementById('homelink').className = 'header-text';
-  document.getElementById('bookmarkslink').className = 'header-text-active';;
+  document.getElementById('bookmarkslink').className = 'header-text header-text-active';;
 }
 
 function headerchangecomments()
 {
   document.getElementById('homelink').className = 'header-text';
-  document.getElementById('commentslink').className = 'header-text-active';
+  document.getElementById('commentslink').className = 'header-text header-text-active';
 }
 
 function headerchangepictures()
 {
   document.getElementById('homelink').className = 'header-text';
-  document.getElementById('pictureslink').className = 'header-text-active';
+  document.getElementById('pictureslink').className = 'header-text header-text-active';
 }
 
 function headerchangeuser()
 {
   document.getElementById('homelink').className = 'header-text';
-  document.getElementById('userlink').className = 'header-text-active';
+  document.getElementById('userlink').className = 'header-text header-text-arrow header-text-active hvr-icon-hang';
+  document.getElementById('sub-header-user-layout-out').id = 'sub-header-user-layout-out-active';
 }
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,6 @@ class SiteController < ApplicationController
   def index
     @bookmarks = Bookmark.last(10)
     @comments = Comment.last(10)
-    @picturespageone = Picture.last(4)
+    @picturespageone = Picture.last(2)
   end
 end
